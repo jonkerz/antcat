57203753429e52256eb151700252177b95e1e72a
Fredrik Palmkron <jonkerz@gmail.com>
2022-02-05 23:50:38 +0100

    staging only: skip some migrations

--- db/migrate/20210707172135_add_not_null_to_reference_documents_reference_id.rb
+++ db/migrate/20210707172135_add_not_null_to_reference_documents_reference_id.rb
@@ -2,8 +2,8 @@
 
 class AddNotNullToReferenceDocumentsReferenceId < ActiveRecord::Migration[6.1]
   def change
-    safety_assured do
-      change_column_null :reference_documents, :reference_id, false
-    end
+    # safety_assured do
+    #   change_column_null :reference_documents, :reference_id, false
+    # end
   end
 end
--- db/migrate/20210707172450_add_foreign_key_to_reference_documents_reference_id.rb
+++ db/migrate/20210707172450_add_foreign_key_to_reference_documents_reference_id.rb
@@ -2,8 +2,8 @@
 
 class AddForeignKeyToReferenceDocumentsReferenceId < ActiveRecord::Migration[6.1]
   def change
-    safety_assured do
-      add_foreign_key :reference_documents, :references, name: :fk_reference_documents__reference_id__references__id
-    end
+    # safety_assured do
+    #   add_foreign_key :reference_documents, :references, name: :fk_reference_documents__reference_id__references__id
+    # end
   end
 end
